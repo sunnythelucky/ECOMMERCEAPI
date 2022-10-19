24c8ff6c5ce3a59ef0e77083f95e6e524c99bcdc
sunnythelucky <sunnyparkk@gmail.com>
2022-10-19 10:54:38 -0400

    add get all products method

--- routes/product.js
+++ routes/product.js
@@ -1,3 +1,14 @@
 const router = require("express").Router();
 
+//GET ALL PRODUCTS
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+	const query = req.query.new;
+	try {
+		const users = query ? await Product.find().sort({ _id: -1 }).limit(1) : await Product.find();
+		res.status(200).json(users);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 module.exports = router;
